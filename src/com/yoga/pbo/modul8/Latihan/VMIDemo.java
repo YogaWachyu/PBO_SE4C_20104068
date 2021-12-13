4439989678c19723c30e40adffcdc588ae2f3250
YogaWachyu <pratama9l15@gmail.com>
2021-12-13 19:25:56 +0700

    Tugas Praktikum PBO modul 8

---
+++ src/com/yoga/pbo/modul8/Latihan/VMIDemo.java
@@ -0,0 +1,13 @@
+package com.yoga.pbo.modul8.Latihan;
+
+public class VMIDemo {
+
+    public static void main(String[] args) {
+        salary s = new salary("Wahyu", "Kubar", 3, 5000);
+        s.checkMail();
+
+        employee e = new salary("Asep", "Jabar", 4, 2500);
+        e.checkMail();
+    }
+}
+
